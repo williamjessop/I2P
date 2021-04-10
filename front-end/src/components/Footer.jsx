2b52f8d46ac507ac7db3d56c897cb80a3c5f8dc0
William Jessop <williamjessop12@outlook.com>
2021-04-10 11:09:38 -0600

    worked on homepage

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Footer</h1>
+                <img src="" alt="mdxlogo"/>
+            </div>
+        );
+    }
+}
+
+export default Footer;
--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,14 +1,79 @@
 import React from "react";
-import './SubmitQuizPopup';
-import SubmitQuizPopup from "./SubmitQuizPopup";
+import { Container } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Footer from './Footer'
 
 class Home extends React.Component {
     render() {
         return (
             <div>
-                <br/><br/>
-                <p>home</p>
-                <SubmitQuizPopup/>
+                <Carousel indicators={false}>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/bannerImage.svg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/bannerImage.svg"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                
+                </Carousel>
+                <Container className="secondaryBackground" fluid>
+                    <br/>
+                    <h1>Prevention</h1>
+                    <p>Fill this spot with the prevention text</p>
+                    <img src="" alt="preventionimg"/>
+                    <Row><br/></Row>
+                </Container>
+                
+                <Container fluid>
+                    <br/>
+                    <h1>Education</h1>
+                    <br/>
+                    <Row>
+                        <Col><img src="" alt="graph"/></Col>
+                        <Col><p>Fill this spot with the education text</p></Col>
+                    </Row>
+                    <br/>
+                    <Row>
+                        <Col><p>Fill this spot with more education text</p></Col>
+                        <Col><img src="" alt="graphic"/></Col>
+                    </Row>
+                    <br/>
+                    <Button size="lg" href="#/education">Get Educated</Button>
+                    <br/>
+                </Container>
+                <br/>
+                <Container className="secondaryBackground" fluid>
+                    <br/>
+                    <h1>Communication</h1>
+                    <Row>
+                        <Col><p>Fill this spot with the prevention text</p></Col>
+                        <Col><img src="" alt="preventionimg"/></Col>
+                    </Row>
+                    <br/>
+                    <Button size="lg" href="#/education">Link to Reach Out</Button>
+                    <Row><br/></Row>
+                </Container>
+                <br/>
+                <Footer/>
             </div>
         );
     }
