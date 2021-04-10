3f5ff14616fc4d7afd354836ce8b6a312e327ee3
Jacob Hansen <thatoneheros451@gmail.com>
2021-04-10 03:17:15 -0600

    Navbar update - content organization (still vertical)

--- front-end/src/components/Navigation.jsx
+++ front-end/src/components/Navigation.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import * as Icon from 'react-bootstrap-icons';
 import './theme.scss';
 
 function Navigation() {
-    return (
-      <div className="navContainerDiv">
-        <Navbar sticky="top" collapseOnSelect expand="md" variant="light" className="navDropShadow">
-          <Navbar.Brand href="#home">
+  return (
+    <div className="navContainerDiv">
+      <Navbar sticky="top" collapseOnSelect expand="md" variant="light" className="navDropShadow">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="#home">
+          <div>
             <img
-                alt=""
-                src="/logo.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
             />{' '}
             Let's Talk
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#prevention">Prevention</Nav.Link>
-              <Nav.Link href="#communication">Communication</Nav.Link>
-              <NavDropdown title="Education" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#course/coursename1">Course 1</NavDropdown.Item>
-                <NavDropdown.Item href="#course/coursename2">Course 2</NavDropdown.Item>
-                <NavDropdown.Item href="#course/coursename3">Course 3</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#faq">Need Help?</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#aboutus">About Us</Nav.Link>
-              <Nav.Link eventKey={2} href="#resources">
-                Resources
+            </div>
+        </Navbar.Brand>
+        <NavDropdown eventKey={3} title={<Icon.PersonCircle />} id="collasible-nav-dropdown btn-profile">
+        </NavDropdown>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#prevention">Prevention</Nav.Link>
+            <Nav.Link href="#communication">Communication</Nav.Link>
+            <NavDropdown title="Education" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#course/coursename1">Course 1</NavDropdown.Item>
+              <NavDropdown.Item href="#course/coursename2">Course 2</NavDropdown.Item>
+              <NavDropdown.Item href="#course/coursename3">Course 3</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#faq">Need Help?</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#aboutus">About Us</Nav.Link>
+            <Nav.Link eventKey={2} href="#resources">
+              Resources
               </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
 
 export default Navigation;
