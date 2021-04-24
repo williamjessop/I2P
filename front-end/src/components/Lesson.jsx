b703bf83be4dfd44844a3a00c28cc47faca148ec
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-04-23 22:47:16 -0600

    Lessons

--- front-end/src/components/Lesson.jsx
+++ front-end/src/components/Lesson.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Col, Row } from "react-bootstrap";
 
 function Lesson(props) {
   return (
@@ -11,16 +11,27 @@ function Lesson(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Lesson {props.currentLesson}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <h4>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </h4>
+        <p>Current pages done: {props.progress}</p>
+        <Row>
+        <Col md={{span: 2 }}><Button>⇦</Button></Col>
+        <Col md={{ span: 2}}><Button>⇨</Button></Col>
+        </Row>
+        
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
