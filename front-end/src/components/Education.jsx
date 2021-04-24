15f197b4e9b2d4181af3032436af724ca538b5d7
Jacob Hansen <thatoneheros451@gmail.com>
2021-04-23 20:27:03 -0600

    Adding education dash

---
+++ front-end/src/components/Education.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button, Card } from 'react-bootstrap';
+
+class Education extends React.Component {
+    //fetch the state from the DB
+    constructor(props) {
+        super(props);
+        this.state = {
+            lessons: [
+                {
+                    pages: [
+                        { content: "", img: "" },
+                        { content: "", img: "" },
+                        { content: "", img: "" }
+                    ],
+                    progress: 0,
+                    visible: true
+                }
+            ],
+            quizAttempts: [
+                {
+                    pages: [
+                        { question: "", answers: ["one", "two", "three", "four"], choice: 0 },
+                        { question: "", answers: ["one", "two", "three", "four"], choice: 0 },
+                        { question: "", answers: ["one", "two", "three", "four"], choice: 0 }
+                    ]
+                }
+            ]
+        };
+    }
+
+
+
+    render() {
+        return (
+            <div className="dash-container">
+                <div className="dash-lesson">
+                    <Card className="dash-lesson-card">
+                        <div className="dash-lesson-card-content dash-lesson-header">
+                            <div className="dash-lesson-title">
+                                Lesson 1
+                                </div>
+                            <div className="dash-lesson-title-button">
+                                <Button>Begin Lesson</Button>
+                            </div>
+                        </div>
+                        <div className="dash-lesson-card-content dash-lesson-content">
+                            <div className="dash-lesson-description">
+                                This lesson covers lesson content in a lesson-like manner, rendering a lesson (how many times can I say lesson in this lesson example description lesson).
+                                </div>
+                            <div className="dash-lesson-description-button">
+                                <Button>Begin Lesson 1 Quiz</Button>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Education;
